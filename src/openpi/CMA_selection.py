9b59b709c71c98b996700decf7b9458932ff1d9e
AndreasL9z <yusenluo@usc.edu>
2025-09-22 08:10:55 +0000

    config

--- src/openpi/CMA_selection.py
+++ src/openpi/CMA_selection.py
@@ -457,7 +457,7 @@ if __name__ == "__main__":
     from openpi.policies import policy_config
     # Paths to your data
     
-    TASK_ATTN_H5 = "/scr2/yusenluo/openpi_debug/openpi/attention_dataset/PI0DROID_remove_marker_from_mug_20_state_first_action.h5"
+    TASK_ATTN_H5 = "/scr2/yusenluo/openpi_debug/openpi/attention_dataset/PI0DROID_place_marker_in_mug_20_state_first_action.h5"
     config = config.get_config("pi0_droid")
     checkpoint_dir = download.maybe_download("gs://openpi-assets/checkpoints/pi0_droid")
     # Ensure normalization assets are present (includes droid/norm_stats.json)
@@ -478,6 +478,8 @@ if __name__ == "__main__":
         "/scr2/yusenluo/openpi_debug/openpi/attention_dataset/PI0DROID_pick_up_red_cube_20_state_first_action.h5",
         "/scr2/yusenluo/openpi_debug/openpi/attention_dataset/PI0DROID_place_green_cube_in_red_bowl_20_state_first_action.h5",
         "/scr2/yusenluo/openpi_debug/openpi/attention_dataset/PI0DROID_wipe_table_with_cloth_20_state_first_action.h5",
+        "/scr2/yusenluo/openpi_debug/openpi/attention_dataset/PI0DROID_wipe_table_with_yellow_cloth_20_state_first_action.h5",
+        "/scr2/yusenluo/openpi_debug/openpi/attention_dataset/PI0DROID_remove_marker_from_mug_20_state_first_action.h5",
     ],
     "/scr2/yusenluo/openpi_debug/openpi/attention_dataset/CMA_baseline_agg_state.h5",
     )
@@ -486,8 +488,8 @@ if __name__ == "__main__":
     baseline_attn_h5=BASELINE_ATTN_H5,
     task_attn_h5=TASK_ATTN_H5,
     policy=policy,                            # 已创建好的 policy
-    raw_h5_path="/scr2/yusenluo/openpi_debug/openpi/on_robot_dataset/centercropped/remove_marker_from_mug_20.h5",    # 就是 generate_activation 使用的原始 h5
-    prompt_text="remove marker from mug",     # 与生成时一致
+    raw_h5_path="/scr2/yusenluo/openpi_debug/openpi/on_robot_dataset/centercropped/place_marker_in_mug_20.h5",    # 就是 generate_activation 使用的原始 h5
+    prompt_text="place marker in mug",     # 与生成时一致
     delta_token_index=0,                      # 注入 state
     num_heads=20,                          # 可选限量
     max_frames=300,
